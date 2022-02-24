54eceae053817b7fd29abf35e8fb3a6bec08ea1b
luc.manzetti <luc.manzetti@hexagon.com>
2022-02-24 18:41:45 +0100

    MapControl

--- WPF_GalacticRunSolver/SolverApp/SolverApp/ViewModels/SolverViewModel.cs
+++ WPF_GalacticRunSolver/SolverApp/SolverApp/ViewModels/SolverViewModel.cs
@@ -13,25 +13,20 @@ namespace SolverApp.ViewModels
         public SolverViewModel()
         {
             theMap = new MapViewModel(16);
-            testTarget = theMap._Target;
-            _TEST = new Command(TEST);
-            testItems = theMap._Cases;
-            testRobots = theMap._Robots;
+
+            _SolveMap = new Command(SolveMap);
 
             testSolutions = new ObservableCollection<SolutionViewModel>();
         }
 
-        public ObservableCollection<ObservableCollection<CaseViewModel>> testItems { get; set; }
-
-        public ObservableCollection<RobotViewModel> testRobots { get; set; }
-
         public ObservableCollection<SolutionViewModel> testSolutions { get; set; }
 
-        public MapViewModel theMap;
-        public TargetViewModel testTarget { get; set; }
+        public MapViewModel theMap { get; set; }
 
-        public Command _TEST { get; }
-        public void TEST()
+
+
+        public Command _SolveMap { get; }
+        public void SolveMap()
         {
             ObservableCollection<SolutionViewModel> result =
                 new ObservableCollection<SolutionViewModel>
